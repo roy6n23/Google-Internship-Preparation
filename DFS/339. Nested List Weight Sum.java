d80afa5a1fd514f0cc73efe98ddc308a8569e767
Edu Roy <1610132@mail.nankai.edu.cn>
2021-01-06 14:56:41 +0800

    second DFS using iterative and recursive

--- DFS/339. Nested List Weight Sum.java
+++ DFS/339. Nested List Weight Sum.java
@@ -26,8 +26,153 @@
  *     public List<NestedInteger> getList();
  * }
  */
+
+//===================DFS RECURSIVE=================
 class Solution {
     public int depthSum(List<NestedInteger> nestedList) {
-        
+        return depthSum(nestedList, 1);
     }
+
+    public int depthSum(List<nestedList> list, int depth){
+    	int sum = 0;
+    	for(NestedInteger n : lists){
+    		if(n.isInteger()){
+    			sum += n*depth;
+    		}else{
+    			//getList相当于解除了嵌套，将外面的括号去掉了
+    			sum += depthSum(n.getList, depth+1);
+    		}
+    	}
+
+    	return sum;
+    }
+
+    //=================BFS iterative
+
+    public int depthSum(List<NestedInteger> nestedList) {
+	    if(nestedList == null){
+	        return 0;
+	    }
+	    
+	    int sum = 0;
+	    int level = 1;
+	    
+	    Queue<NestedInteger> queue = new LinkedList<NestedInteger>(nestedList);
+	    while(queue.size() > 0){
+	        int size = queue.size();
+	        
+	        for(int i = 0; i < size; i++){
+	            NestedInteger ni = queue.poll();
+	            
+	            if(ni.isInteger()){
+	                sum += ni.getInteger() * level;
+	            }else{
+	                queue.addAll(ni.getList());
+	            }
+	        }
+	        
+	        level++;
+	    }
+	    
+	    return sum;
+	}
+
+
+	//=================DFS   iterative
+
+	class NestedIntegerD{
+        NestedInteger ni;
+        int depth;
+        NestedIntegerD(NestedInteger ni, int depth){
+            this.ni = ni;
+            this.depth = depth;            
+        }
+    }
+    
+    public int depthSum(List<NestedInteger> nestedList) {
+        Stack<NestedIntegerD> st = new Stack<>();
+        for(int i=nestedList.size()-1; i>=0; i--){
+            NestedInteger ni = nestedList.get(i);
+            NestedIntegerD nid = new NestedIntegerD(ni, 1);
+            st.push(nid);
+        }
+        
+        int sum = 0;
+        while(!st.isEmpty())
+        {
+            NestedIntegerD nid = st.pop();
+            if(nid.ni.isInteger())
+                sum+=nid.ni.getInteger()*nid.depth;
+            else
+            {
+                for(NestedInteger item:nid.ni.getList())
+                {
+                    st.push(new NestedIntegerD(item, nid.depth+1));
+                }
+            }
+        }
+        return sum;
+    }
+
+
+    //================BFS ITERATIVE
+
+
+    public class Solution {
+    public int depthSum(List<NestedInteger> nestedList) {
+        if (nestedList == null || nestedList.size() == 0) {
+            return 0;
+        }
+        int sum = 0;
+        Queue<NestedInteger> queue = new LinkedList<>();
+        for (NestedInteger nestedInt : nestedList) {
+            queue.offer(nestedInt);
+        }
+        
+        int depth = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            depth++;
+            for (int i = 0; i < size; i++) {
+                NestedInteger nestedInt = queue.poll();
+                if (nestedInt.isInteger()) {
+                    sum += nestedInt.getInteger() * depth;
+                } else {
+                    for (NestedInteger innerInt : nestedInt.getList()) {
+                        queue.offer(innerInt);
+                    }
+                }
+            }
+        }
+        return sum;
+    }
+}
+
+
+//=========DFS
+
+public class Solution {
+    private int sum;
+    public int depthSum(List<NestedInteger> nestedList) {
+        if (nestedList == null) {
+            return 0;
+        }
+        sum = 0;
+        for (NestedInteger nestedInt : nestedList) {
+            depthSumHelper(nestedInt, 1);
+        }
+        return sum;
+    }
+    
+    private void depthSumHelper(NestedInteger nestedInt, int depth) {
+        if (nestedInt.isInteger()) {
+            sum += depth * nestedInt.getInteger();
+            return;
+        }
+        for (NestedInteger innerInt : nestedInt.getList()) {
+            depthSumHelper(innerInt, depth + 1);
+        }
+    }
+}
+
 }
